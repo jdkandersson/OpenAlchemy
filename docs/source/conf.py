a7411156d31ad3f1d42efb15e5ddd1569a07f821
David Andersson <anderssonpublic@gmail.com>
2019-09-08 16:35:25 +1000

    Fixed linting problems

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,3 +1,4 @@
+"""Documentation configuration."""
 # Configuration file for the Sphinx documentation builder.
 #
 # This file only contains a selection of the most common options. For a full
@@ -17,12 +18,12 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'openapi-SQLAlchemy'
-copyright = '2019, David Andersson'
-author = 'David Andersson'
+project = "openapi-SQLAlchemy"
+copyright = "2019, David Andersson"
+author = "David Andersson"
 
 # The full version, including alpha/beta/rc tags
-release = '0.2.0'
+release = "0.2.0"
 
 
 # -- General configuration ---------------------------------------------------
@@ -30,11 +31,10 @@ release = '0.2.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -47,11 +47,11 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "alabaster"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
-master_doc = 'index'
+master_doc = "index"
