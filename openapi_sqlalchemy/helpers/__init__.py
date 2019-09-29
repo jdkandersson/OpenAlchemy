f792ce59b91abc3c5ed8fd9d7e2ae81d3cfb50fe
David Andersson <anderssonpublic@gmail.com>
2019-09-29 15:17:08 +1000

    Removed redundant helper

--- openapi_sqlalchemy/helpers/__init__.py
+++ openapi_sqlalchemy/helpers/__init__.py
@@ -1,6 +1,5 @@
 """Helper functions."""
 
-from .add_logical_name import add_logical_name
 from .merge_all_of import merge_all_of
 from .prepare_spec import prepare_spec
 from .resolve_ref import resolve_ref
