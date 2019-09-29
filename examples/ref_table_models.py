545d738710e3654d3b97e050708c9789f3a38bf7
David Andersson <anderssonpublic@gmail.com>
2019-09-29 15:40:32 +1000

    Added example for referencing tables

---
+++ examples/ref_table_models.py
@@ -0,0 +1,12 @@
+from sqlalchemy.ext.declarative import declarative_base
+from yaml import safe_load
+
+from openapi_sqlalchemy import init_model_factory
+
+Base = declarative_base()
+with open("ref-table-example-spec.yml") as spec_file:
+    SPEC = safe_load(spec_file)
+MODEL_FACTORY = init_model_factory(base=Base, spec=SPEC)
+
+
+Employee = MODEL_FACTORY(name="Employee")
