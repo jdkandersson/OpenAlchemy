e126dc8d88f7071f3b7c29cf4a3a23b8d0425fe7
David Andersson <anderssonpublic@gmail.com>
2019-10-19 15:44:09 +1100

    Add new resolve_ref function that keeps track of name

--- openapi_sqlalchemy/helpers/__init__.py
+++ openapi_sqlalchemy/helpers/__init__.py
@@ -3,4 +3,5 @@
 from .merge_all_of import merge_all_of
 from .prepare_spec import prepare_spec
 from .resolve_ref import legacy_resolve_ref
+from .resolve_ref import resolve_ref
 from .testing_guard import testing_guard
--- openapi_sqlalchemy/helpers/resolve_ref.py
+++ openapi_sqlalchemy/helpers/resolve_ref.py
@@ -1,6 +1,7 @@
 """Used to resolve schema references."""
 
 import re
+import typing
 
 from openapi_sqlalchemy import exceptions
 from openapi_sqlalchemy import types
@@ -40,3 +41,47 @@ def legacy_resolve_ref(*, schema: types.Schema, schemas: types.Schemas) -> types
 
     new_schema = types.Schema(schema_name, ref_schema)
     return legacy_resolve_ref(schema=new_schema, schemas=schemas)
+
+
+def resolve_ref(
+    *,
+    name: str,
+    schema: typing.Dict[str, typing.Any],
+    schemas: typing.Dict[str, typing.Dict[str, typing.Any]],
+) -> typing.Tuple[str, types.SchemaSpec]:
+    """
+    Resolve reference to another schema.
+
+    Recursively resolves $ref until $ref key is no longer found. On each step, the name
+    of the schema is recorded.
+
+    Raises SchemaNotFound is a $ref resolution fails.
+
+    Args:
+        name: The name of the schema from the last step.
+        schema: The specification of the schema from the last step.
+        schemas: Dictionary with all defined schemas used to resolve $ref.
+
+    Returns:
+        The first schema that no longer has the $ref key and the name of that schema.
+
+    """
+    # Checking whether schema is a reference schema
+    ref = schema.get("$ref")
+    if ref is None:
+        return name, schema
+
+    # Checking value of $ref
+    match = _REF_PATTER.match(ref)
+    if not match:
+        raise exceptions.SchemaNotFoundError(
+            f"{ref} format incorrect, expected #/components/schemas/<SchemaName>"
+        )
+
+    # Retrieving new schema
+    ref_name = match.group(1)
+    ref_schema = schemas.get(ref_name)
+    if ref_schema is None:
+        raise exceptions.SchemaNotFoundError(f"{ref_name} was not found in schemas.")
+
+    return resolve_ref(name=ref_name, schema=ref_schema, schemas=schemas)
