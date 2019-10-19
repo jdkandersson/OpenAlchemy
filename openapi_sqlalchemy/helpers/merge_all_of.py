a0b00700c7c74d7b46c112d064e86805df336e15
David Andersson <anderssonpublic@gmail.com>
2019-10-19 16:07:20 +1100

    Add new merge_all_of using new resolve_ref

--- openapi_sqlalchemy/helpers/merge_all_of.py
+++ openapi_sqlalchemy/helpers/merge_all_of.py
@@ -1,8 +1,11 @@
 """Merges objects under allOf statement."""
 
+import typing
+
 from openapi_sqlalchemy import types
 
 from .resolve_ref import legacy_resolve_ref
+from .resolve_ref import resolve_ref
 
 
 def legacy_merge_all_of(
@@ -52,3 +55,52 @@ def legacy_merge_all_of(
         merged_spec["required"] = list(required_set)
 
     return merged_spec
+
+
+def merge_all_of(
+    *,
+    schema: typing.Dict[str, typing.Any],
+    schemas: typing.Dict[str, typing.Dict[str, typing.Any]]
+) -> typing.Dict[str, typing.Any]:
+    """
+    Merge schemas under allOf statement.
+
+    Merges schemas under allOf statement which is expected to have a list of schemas.
+    Any duplicate keys will be overridden. Schemas are processed in the order they are
+    listed.
+
+    Args:
+        schema: The schema to operate on.
+        schemas: Used to resolve any $ref.
+
+    Returns:
+        The schema with all top level allOf statements resolved.
+
+    """
+    all_of = schema.get("allOf")
+    if all_of is None:
+        return schema
+
+    merged_schema: typing.Dict[str, typing.Any] = {}
+    for sub_schema in all_of:
+        # Resolving any $ref
+        _, ref_schema = resolve_ref(name="", schema=sub_schema, schemas=schemas)
+        # Merging any nested allOf
+        merged_sub_schema = merge_all_of(schema=ref_schema, schemas=schemas)
+
+        # Capturing required arrays
+        merged_required = merged_schema.get("required")
+        sub_required = merged_sub_schema.get("required")
+
+        # Combining sub into merged specification
+        merged_schema = {**merged_schema, **merged_sub_schema}
+
+        # Checking whether required was present on both specs
+        if merged_required is None or sub_required is None:
+            continue
+
+        # Both have a required array, need to merge them together with common elements
+        required_set = set(merged_required).union(sub_required)
+        merged_schema["required"] = list(required_set)
+
+    return merged_schema
--- tests/helpers/test_merge_all_of.py
+++ tests/helpers/test_merge_all_of.py
@@ -7,7 +7,7 @@ from openapi_sqlalchemy import helpers
 
 
 @pytest.mark.helper
-def test_not_all_of():
+def test_legacy_not_all_of():
     """
     GIVEN spec that does not have the allOf statement
     WHEN legacy_merge_all_of is called with the spec
@@ -21,7 +21,7 @@ def test_not_all_of():
 
 
 @pytest.mark.helper
-def test_single():
+def test_legacy_single():
     """
     GIVEN spec that has allOf statement with a single spec
     WHEN legacy_merge_all_of is called with the spec
@@ -35,7 +35,7 @@ def test_single():
 
 
 @pytest.mark.helper
-def test_multiple():
+def test_legacy_multiple():
     """
     GIVEN spec that has multiple specs under allOf
     WHEN legacy_merge_all_of is called with the spec
@@ -49,7 +49,7 @@ def test_multiple():
 
 
 @pytest.mark.helper
-def test_multiple_same_key():
+def test_legacy_multiple_same_key():
     """
     GIVEN spec that has multiple specs under allOf with the same key
     WHEN legacy_merge_all_of is called with the spec
@@ -63,7 +63,7 @@ def test_multiple_same_key():
 
 
 @pytest.mark.helper
-def test_nested_all_of():
+def test_legacy_nested_all_of():
     """
     GIVEN spec that has allOf statement with an allOf statement with a single spec
     WHEN legacy_merge_all_of is called with the spec
@@ -77,7 +77,7 @@ def test_nested_all_of():
 
 
 @pytest.mark.helper
-def test_ref():
+def test_legacy_ref():
     """
     GIVEN spec that has allOf statement with $ref to another spec
     WHEN legacy_merge_all_of is called with the spec
@@ -92,7 +92,7 @@ def test_ref():
 
 
 @pytest.mark.helper
-def test_ref_all_of():
+def test_legacy_ref_all_of():
     """
     GIVEN spec that has allOf statement with $ref to another spec with an allOf
         statement with a spec
@@ -118,7 +118,7 @@ def test_ref_all_of():
     ids=["first only", "second only", "different", "common"],
 )
 @pytest.mark.helper
-def test_ref_all_required(all_of_spec, expected_required):
+def test_legacy_ref_all_required(all_of_spec, expected_required):
     """
     GIVEN spec that has allOf with specs with given required properties and expected
         final required
@@ -131,3 +131,130 @@ def test_ref_all_required(all_of_spec, expected_required):
     return_spec = helpers.legacy_merge_all_of(spec=spec, schemas=schemas)
 
     assert sorted(return_spec["required"]) == sorted(expected_required)
+
+
+@pytest.mark.helper
+def test_not_all_of():
+    """
+    GIVEN schema that does not have the allOf statement
+    WHEN merge_all_of is called with the schema
+    THEN the schema is returned.
+    """
+    schema = {"key": "value"}
+
+    return_schema = helpers.merge_all_of(schema=schema, schemas={})
+
+    assert return_schema == {"key": "value"}
+
+
+@pytest.mark.helper
+def test_single():
+    """
+    GIVEN schema that has allOf statement with a single schema
+    WHEN merge_all_of is called with the schema
+    THEN the schema in allOf is returned.
+    """
+    schema = {"allOf": [{"key": "value"}]}
+
+    return_schema = helpers.merge_all_of(schema=schema, schemas={})
+
+    assert return_schema == {"key": "value"}
+
+
+@pytest.mark.helper
+def test_multiple():
+    """
+    GIVEN schema that has multiple schemas under allOf
+    WHEN merge_all_of is called with the schema
+    THEN the merged schema of all schemas under allOf is returned.
+    """
+    schema = {"allOf": [{"key_1": "value_1"}, {"key_2": "value_2"}]}
+
+    return_schema = helpers.merge_all_of(schema=schema, schemas={})
+
+    assert return_schema == {"key_1": "value_1", "key_2": "value_2"}
+
+
+@pytest.mark.helper
+def test_multiple_same_key():
+    """
+    GIVEN schema that has multiple schemas under allOf with the same key
+    WHEN merge_all_of is called with the schema
+    THEN the value of the last schema is assigned to the key in the returned schema.
+    """
+    schema = {"allOf": [{"key": "value_1"}, {"key": "value_2"}]}
+
+    return_schema = helpers.merge_all_of(schema=schema, schemas={})
+
+    assert return_schema == {"key": "value_2"}
+
+
+@pytest.mark.helper
+def test_nested_all_of():
+    """
+    GIVEN schema that has allOf statement with an allOf statement with a single schema
+    WHEN merge_all_of is called with the schema
+    THEN the schema in allOf is returned.
+    """
+    schema = {"allOf": [{"allOf": [{"key": "value"}]}]}
+
+    return_schema = helpers.merge_all_of(schema=schema, schemas={})
+
+    assert return_schema == {"key": "value"}
+
+
+@pytest.mark.helper
+def test_ref():
+    """
+    GIVEN schema that has allOf statement with $ref to another schema
+    WHEN merge_all_of is called with the schema
+    THEN the $ref schema in allOf is returned.
+    """
+    schema = {"allOf": [{"$ref": "#/components/schemas/RefSchema"}]}
+    schemas = {"RefSchema": {"key": "value"}}
+
+    return_schema = helpers.merge_all_of(schema=schema, schemas=schemas)
+
+    assert return_schema == {"key": "value"}
+
+
+@pytest.mark.helper
+def test_ref_all_of():
+    """
+    GIVEN schema that has allOf statement with $ref to another schema with an allOf
+        statement with a schema
+    WHEN merge_all_of is called with the schema
+    THEN the allOf $ref schema in allOf is returned.
+    """
+    schema = {"allOf": [{"$ref": "#/components/schemas/RefSchema"}]}
+    schemas = {"RefSchema": {"allOf": [{"key": "value"}]}}
+
+    return_schema = helpers.merge_all_of(schema=schema, schemas=schemas)
+
+    assert return_schema == {"key": "value"}
+
+
+@pytest.mark.parametrize(
+    "all_of_schema, expected_required",
+    [
+        ([{"required": ["id"]}, {}], ["id"]),
+        ([{}, {"required": ["id"]}], ["id"]),
+        ([{"required": ["id"]}, {"required": ["name"]}], ["id", "name"]),
+        ([{"required": ["id"]}, {"required": ["id"]}], ["id"]),
+    ],
+    ids=["first only", "second only", "different", "common"],
+)
+@pytest.mark.helper
+def test_ref_all_required(all_of_schema, expected_required):
+    """
+    GIVEN schema that has allOf with schemas with given required properties and expected
+        final required
+    WHEN merge_all_of is called with the schema
+    THEN the returned schema has the expected required property.
+    """
+    schema = {"allOf": all_of_schema}
+    schemas = {}
+
+    return_schema = helpers.merge_all_of(schema=schema, schemas=schemas)
+
+    assert sorted(return_schema["required"]) == sorted(expected_required)
