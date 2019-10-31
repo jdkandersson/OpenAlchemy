19cff28c5511fa409bc66572edf779899c08d2cc
David Andersson <anderssonpublic@gmail.com>
2019-11-01 08:31:56 +1100

    Update example to use new sytnax

--- examples/app/models.py
+++ examples/app/models.py
@@ -3,19 +3,16 @@
 import os
 
 from flask_sqlalchemy import SQLAlchemy
-from yaml import safe_load
 
-from openapi_sqlalchemy import init_model_factory
+from openapi_sqlalchemy import init_yaml
 
 db = SQLAlchemy()
 
 models_dir = os.path.dirname(__file__)
-with open(os.path.join(models_dir, "api.yaml")) as spec_file:
-    SPEC = safe_load(spec_file)
-MODEL_FACTORY = init_model_factory(base=db.Model, spec=SPEC)
+_, model_factory = init_yaml(os.path.join(models_dir, "api.yaml"), base=db.Model)
 
 
-class Employee(MODEL_FACTORY(name="Employee")):  # type: ignore
+class Employee(model_factory(name="Employee")):  # type: ignore
     """Employee model with conversion to and from dictionaries."""
 
     @classmethod
