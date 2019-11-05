36daf328f7c0e26e2972ffdf5693d8a32d56fb70
David Andersson <anderssonpublic@gmail.com>
2019-11-05 16:01:08 +1100

    Add to and from dict test with relationship

--- tests/open_alchemy/test_integration.py
+++ tests/open_alchemy/test_integration.py
@@ -180,7 +180,7 @@ def test_schema():
 @pytest.mark.integration
 def test_to_from_dict():
     """
-    GIVEN specification with single property
+    GIVEN specification that has schema with a single property
     WHEN model is defined based on schema and constructed using from_dict
     THEN when to_dict is called the construction dictionary is returned.
     """
@@ -202,6 +202,44 @@ def test_to_from_dict():
 
     # COnstructing and turning back to dictionary
     model_dict = {"column": 1}
+    instance = model.from_dict(**model_dict)
+    assert instance.to_dict() == model_dict
+
+
+@pytest.mark.prod_env
+@pytest.mark.integration
+def test_to_from_dict_relationship():
+    """
+    GIVEN specification that has a schema with a relationship
+    WHEN model is defined based on schema and constructed using from_dict
+    THEN when to_dict is called the construction dictionary is returned.
+    """
+    model_factory = open_alchemy.init_model_factory(
+        base=mock.MagicMock,
+        spec={
+            "components": {
+                "schemas": {
+                    "RefTable": {
+                        "properties": {"id": {"type": "integer"}},
+                        "x-tablename": "table",
+                        "type": "object",
+                    },
+                    "Table": {
+                        "properties": {
+                            "column": {"$ref": "#/components/schemas/RefTable"}
+                        },
+                        "x-tablename": "table",
+                        "type": "object",
+                    },
+                }
+            }
+        },
+    )
+    model_factory(name="RefTable")
+    model = model_factory(name="Table")
+
+    # COnstructing and turning back to dictionary
+    model_dict = {"column": {"id": 1}}
     instance = model.from_dict(**model_dict)
     assert instance.to_dict() == model_dict
 
