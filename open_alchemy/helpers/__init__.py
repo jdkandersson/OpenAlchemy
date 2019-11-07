51d8c594887d299e1759d93d6daba73491ce0784
David Andersson <anderssonpublic@gmail.com>
2019-11-07 14:34:24 +1100

    Remove helper no longer in use

--- open_alchemy/helpers/__init__.py
+++ open_alchemy/helpers/__init__.py
@@ -6,4 +6,3 @@ from .merge_all_of import merge_all_of
 from .peek_type import peek_type
 from .prepare_schema import prepare_schema
 from .resolve_ref import resolve_ref
-from .testing_guard import testing_guard
