0fb6c7c083be38cbdee2d561dd37fa8b7f6650cd
David Andersson <anderssonpublic@gmail.com>
2019-11-09 08:41:32 +1100

    Add support for unique and index table args

--- open_alchemy/table_args/__init__.py
+++ open_alchemy/table_args/__init__.py
@@ -1,1 +1,42 @@
 """Construct table args for a model."""
+
+import itertools
+import typing
+
+from sqlalchemy import schema as sa_schema
+
+from open_alchemy import helpers
+from open_alchemy import types
+
+from . import factory
+
+TableArgs = typing.Tuple[typing.Union[sa_schema.UniqueConstraint, sa_schema.Index], ...]
+
+
+def construct(*, schema: types.Schema) -> TableArgs:
+    """
+    Construct any table args from the object schema.
+
+    Look for x-unique-constraint and x-composite-index keys in the schema and construct
+    any unique constraints and indexes based on their value.
+
+    Args:
+        schema: The schema for the object.
+
+    Returns:
+        A tuple with any unique constraints and indexes.
+
+    """
+    # Keep track of any table arguments
+    table_args = []
+
+    # Handle x-unique-constraint
+    unique_spec = helpers.get_ext_prop(source=schema, name="x-unique-constraint")
+    if unique_spec is not None:
+        table_args.append(factory.unique_factory(spec=unique_spec))
+    # Handle x-composite-index
+    index_spec = helpers.get_ext_prop(source=schema, name="x-composite-index")
+    if index_spec is not None:
+        table_args.append(factory.index_factory(spec=index_spec))
+
+    return tuple(itertools.chain.from_iterable(table_args))
--- tests/open_alchemy/table_args/test_table_args.py
+++ tests/open_alchemy/table_args/test_table_args.py
@@ -1,1 +1,27 @@
 """Tests for table_args functions."""
+
+import pytest
+
+from open_alchemy import table_args
+
+
+@pytest.mark.parametrize(
+    "schema, expected_length",
+    [
+        ({}, 0),
+        ({"x-unique-constraint": ["column 1"]}, 1),
+        ({"x-composite-index": ["column 1"]}, 1),
+        ({"x-unique-constraint": ["column 1"], "x-composite-index": ["column 2"]}, 2),
+    ],
+    ids=["empty", "x-unique-constraint", "x-composite-index", "all"],
+)
+@pytest.mark.table_args
+def test_construct(schema, expected_length):
+    """
+    GIVEN schema and expected length
+    WHEN construct is called with the schema
+    THEN a tuple with the expected length is returned.
+    """
+    returned_args = table_args.construct(schema=schema)
+
+    assert len(returned_args) == expected_length
