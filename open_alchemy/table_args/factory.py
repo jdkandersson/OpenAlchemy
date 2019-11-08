ff9e70b22b78ccfe32611b80bc157b9c517f8e94
David Andersson <anderssonpublic@gmail.com>
2019-11-08 14:22:05 +1100

    Add support for converting unique schemas to consistent format

--- open_alchemy/table_args/factory.py
+++ open_alchemy/table_args/factory.py
@@ -1,8 +1,55 @@
 """Create table args such as UniqueConstraints and Index."""
 
+import json
+import os
 import typing
 
+import jsonschema
+
+from open_alchemy import exceptions
 from open_alchemy import types
+
+_DIRECTORY = os.path.dirname(__file__)
+_PATHS = ("..", "helpers", "get_ext_prop")
+_COMMON_SCHEMAS_FILE = os.path.join(_DIRECTORY, *_PATHS, "common-schemas.json")
+with open(_COMMON_SCHEMAS_FILE) as in_file:
+    _COMMON_SCHEMAS = json.load(in_file)
+_resolver = jsonschema.RefResolver.from_schema(  # pylint: disable=invalid-name
+    _COMMON_SCHEMAS
+)
+
+
+def _spec_to_schema_name(
+    *,
+    spec: types.AnyUniqueConstraint,
+    schema_names: typing.Optional[typing.List[str]] = None,
+) -> str:
+    """
+    Convert a specification to the name of the matched schema.
+
+    Use the schema names defined in common-schemas.json to find the first matching
+    schema.
+
+    Args:
+        spec: The specification to convert.
+        schema_names: The names of the schemas to check.
+
+    Returns:
+        The name of the specification.
+
+    """
+    if schema_names is None:
+        schema_names = _COMMON_SCHEMAS.keys()
+
+    for name in schema_names:
+        try:
+            jsonschema.validate(
+                instance=spec, schema=_COMMON_SCHEMAS[name], resolver=_resolver
+            )
+            return name
+        except jsonschema.ValidationError:
+            continue
+    raise exceptions.SchemaNotFoundError("Specification did not match any schemas.")
 
 
 def _handle_column_list(spec: typing.List[str]) -> types.UniqueConstraint:
@@ -23,18 +70,31 @@ _UNIQUE_MAPPING: typing.Dict[str, typing.Callable[..., types.UniqueConstraintLis
     "ColumnList": lambda spec: [_handle_column_list(spec=spec)],
     "ColumnListList": lambda spec: list(map(_handle_column_list, spec)),
     "UniqueConstraint": lambda spec: [spec],
-    "uniqueConstraintList": lambda spec: spec,
+    "UniqueConstraintList": lambda spec: spec,
 }
 
 
-# def _handle_unique(*, spec: types.AnyUniqueConstraint) -> types.UniqueConstraintList:
-#     """
-#     Convert any unique constraint to UniqueConstraintList.
+_SCHEMAS_FILE = os.path.join(_DIRECTORY, *_PATHS, "extension-schemas.json")
+with open(_SCHEMAS_FILE) as in_file:
+    _SCHEMAS = json.load(in_file)
+_UNIQUE_SCHEMA_NAMES: typing.List[str] = list(
+    map(
+        lambda schema: schema["$ref"].split("/")[-1],
+        _SCHEMAS["x-unique-constraint"]["oneOf"],
+    )
+)
 
-#     Args:
-#         spec: The specification to convert.
 
-#     Returns:
-#         The UniqueConstraintList.
+def _handle_unique(*, spec: types.AnyUniqueConstraint) -> types.UniqueConstraintList:
+    """
+    Convert any unique constraint to UniqueConstraintList.
 
-#     """
+    Args:
+        spec: The specification to convert.
+
+    Returns:
+        The UniqueConstraintList.
+
+    """
+    name = _spec_to_schema_name(spec=spec, schema_names=_UNIQUE_SCHEMA_NAMES)
+    return _UNIQUE_MAPPING[name](spec)
--- tests/open_alchemy/table_args/test_factory.py
+++ tests/open_alchemy/table_args/test_factory.py
@@ -1,38 +1,116 @@
 """Tests for table args factories."""
 
-# import pytest
+import functools
 
-# from open_alchemy.table_args import factory
+import pytest
 
-
-# @pytest.mark.table_args
-# def test_handle_column_list():
-#     """
-#     GIVEN list of columns
-#     WHEN _handle_column_list is called
-#     THEN list of unique constraints is returned.
-#     """
-#     spec = ["column 1"]
-
-#     unique_list = factory._handle_column_list(  # pylint: disable=protected-access
-#         spec=spec
-#     )
-
-#     assert unique_list == [{"columns": ["column 1"]}]
+from open_alchemy import exceptions
+from open_alchemy.table_args import factory
 
 
-# @pytest.mark.parametrize(
-#     "spec",
-#     [[["column 1"]], [["column 1"], ["column 2"]]],
-#     ids=["single", "multiple"],
-# )
-# @pytest.mark.table_args
-# def test_handle_column_list_list(spec):
-#     """
-#     GIVEN list of list of columns
-#     WHEN _handle_column_list_list is called
-#     THEN indexes with columns are returned.
-#     """
-#     indexes = factory._handle_column_list_list(spec=spec)
+@pytest.mark.parametrize(
+    "spec, expected_name",
+    [
+        (["column 1"], "ColumnList"),
+        ([["column 1"]], "ColumnListList"),
+        ({"columns": ["column 1"]}, "UniqueConstraint"),
+        ({"columns": ["column 1"], "name": "name 1"}, "UniqueConstraint"),
+        ([{"columns": ["column 1"]}], "UniqueConstraintList"),
+        ({"name": "name 1", "expressions": ["column 1"]}, "Index"),
+        ({"name": "name 1", "expressions": ["column 1"], "unique": True}, "Index"),
+        ([{"name": "name 1", "expressions": ["column 1"]}], "IndexList"),
+    ],
+    ids=[
+        "ColumnList",
+        "ColumnListList",
+        "UniqueConstraint no name",
+        "UniqueConstraint",
+        "UniqueConstraintList",
+        "Index no unique",
+        "Index",
+        "IndexList",
+    ],
+)
+@pytest.mark.table_args
+def test_spec_to_schema_name(spec, expected_name):
+    """
+    GIVEN spec and expected name
+    WHEN _spec_to_schema_name is called with the spec
+    THEN the expected name is returned
+    """
+    name = factory._spec_to_schema_name(spec=spec)  # pylint: disable=protected-access
 
-#     mapped_indexes = list(map(lambda index: index._pending_colargs))
+    assert name == expected_name
+
+
+@pytest.mark.parametrize(
+    "schema_names, spec, raises",
+    [
+        ([], ["column 1"], True),
+        (["ColumnListList"], ["column 1"], True),
+        (["ColumnList"], ["column 1"], False),
+        (["ColumnListList", "UniqueConstraint"], ["column 1"], True),
+        (["ColumnList", "UniqueConstraint"], ["column 1"], False),
+        (["UniqueConstraint", "ColumnList"], ["column 1"], False),
+    ],
+    ids=[
+        "empty schemas,   -,              raises",
+        "single schemas,   spec diff,     raises",
+        "single schemas,   spec match,    not raises",
+        "multiple schemas, spec not in,   raises",
+        "multiple schemas, spec in,       not raises",
+        "multiple schemas, other spec in, not raises",
+    ],
+)
+@pytest.mark.table_args
+def test_spec_to_name_schemas(schema_names, spec, raises):
+    """
+    GIVEN list of schema names, spec and whether a raise is expected
+    WHEN _spec_to_schema_name is called with the spec and schemas
+    THEN SchemaNotFoundError is raised is a raise is expected.
+    """
+    test_func = functools.partial(
+        factory._spec_to_schema_name,  # pylint: disable=protected-access
+        spec=spec,
+        schema_names=schema_names,
+    )
+
+    if raises:
+        with pytest.raises(exceptions.SchemaNotFoundError):
+            test_func()
+    else:
+        test_func()
+
+
+@pytest.mark.parametrize(
+    "spec, expected_spec",
+    [
+        (["column 1"], [{"columns": ["column 1"]}]),
+        ([["column 1"]], [{"columns": ["column 1"]}]),
+        (
+            [["column 1"], ["column 2"]],
+            [{"columns": ["column 1"]}, {"columns": ["column 2"]}],
+        ),
+        ({"columns": ["column 1"]}, [{"columns": ["column 1"]}]),
+        ([{"columns": ["column 1"]}], [{"columns": ["column 1"]}]),
+    ],
+    ids=[
+        "column list",
+        "column list of list single",
+        "column list of list multiple",
+        "unique constraint",
+        "list of unique constraint",
+    ],
+)
+@pytest.mark.table_args
+def test_handle_unique(spec, expected_spec):
+    """
+    GIVEN specification and expected specification
+    WHEN _handle_unique is called with the specification
+    THEN the expected specification is returned.
+    """
+    returned_spec = factory._handle_unique(  # pylint: disable=protected-access
+        spec=spec
+    )
+
+    assert returned_spec == expected_spec
