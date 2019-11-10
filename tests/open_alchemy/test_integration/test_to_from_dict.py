00b55510c94617e1f1c530311fad6c5b1b1880d3
David Andersson <anderssonpublic@gmail.com>
2019-11-10 17:39:08 +1100

    Add from and to dict integration tests for one to many

--- tests/open_alchemy/test_integration/test_to_from_dict.py
+++ tests/open_alchemy/test_integration/test_to_from_dict.py
@@ -46,9 +46,9 @@ def test_to_from_dict(engine, sessionmaker):
 
 
 @pytest.mark.integration
-def test_to_from_dict_relationship(engine, sessionmaker):
+def test_to_from_dict_many_to_one_relationship(engine, sessionmaker):
     """
-    GIVEN specification that has a schema with a relationship
+    GIVEN specification that has a schema with a many to one relationship
     WHEN model is defined based on schema and constructed using from_dict
     THEN when to_dict is called the construction dictionary is returned.
     """
@@ -85,6 +85,57 @@ def test_to_from_dict_relationship(engine, sessionmaker):
 
     # COnstructing and turning back to dictionary
     model_dict = {"id": 11, "ref_table": {"id": 12}}
+    instance = model.from_dict(**model_dict)
+    session = sessionmaker()
+    session.add(instance)
+    session.flush()
+    queried_instance = session.query(model).first()
+    assert queried_instance.to_dict() == model_dict
+
+
+@pytest.mark.integration
+def test_to_from_dict_one_to_many_relationship(engine, sessionmaker):
+    """
+    GIVEN specification that has a schema with a one to many relationship
+    WHEN model is defined based on schema and constructed using from_dict
+    THEN when to_dict is called the construction dictionary is returned.
+    """
+    # Creating model factory
+    base = declarative.declarative_base()
+    model_factory = open_alchemy.init_model_factory(
+        base=base,
+        spec={
+            "components": {
+                "schemas": {
+                    "RefTable": {
+                        "properties": {
+                            "id": {"type": "integer", "x-primary-key": True}
+                        },
+                        "x-tablename": "ref_table",
+                        "type": "object",
+                    },
+                    "Table": {
+                        "properties": {
+                            "id": {"type": "integer", "x-primary-key": True},
+                            "ref_tables": {
+                                "type": "array",
+                                "items": {"$ref": "#/components/schemas/RefTable"},
+                            },
+                        },
+                        "x-tablename": "table",
+                        "type": "object",
+                    },
+                }
+            }
+        },
+    )
+    model_factory(name="RefTable")
+    model = model_factory(name="Table")
+    # Creating models
+    base.metadata.create_all(engine)
+
+    # COnstructing and turning back to dictionary
+    model_dict = {"id": 11, "ref_tables": [{"id": 12}]}
     instance = model.from_dict(**model_dict)
     session = sessionmaker()
     session.add(instance)
