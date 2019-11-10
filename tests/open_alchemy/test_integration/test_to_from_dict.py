a6d62ad6a962d1879fdb0d9437c2b9c59bc0de8f
David Andersson <anderssonpublic@gmail.com>
2019-11-10 17:36:45 +1100

    Change from and to dict integration tests to include trip to database

--- tests/open_alchemy/test_integration/test_to_from_dict.py
+++ tests/open_alchemy/test_integration/test_to_from_dict.py
@@ -1,26 +1,29 @@
 """Integration tests for from_dict and to_dict."""
 
-from unittest import mock
-
 import pytest
+from sqlalchemy.ext import declarative
 
 import open_alchemy
 
 
 @pytest.mark.integration
-def test_to_from_dict():
+def test_to_from_dict(engine, sessionmaker):
     """
     GIVEN specification that has schema with a single property
     WHEN model is defined based on schema and constructed using from_dict
     THEN when to_dict is called the construction dictionary is returned.
     """
+    # Creating model factory
+    base = declarative.declarative_base()
     model_factory = open_alchemy.init_model_factory(
-        base=mock.MagicMock,
+        base=base,
         spec={
             "components": {
                 "schemas": {
                     "Table": {
-                        "properties": {"column": {"type": "integer"}},
+                        "properties": {
+                            "column": {"type": "integer", "x-primary-key": True}
+                        },
                         "x-tablename": "table",
                         "type": "object",
                     }
@@ -29,33 +32,44 @@ def test_to_from_dict():
         },
     )
     model = model_factory(name="Table")
+    # Creating models
+    base.metadata.create_all(engine)
 
     # COnstructing and turning back to dictionary
     model_dict = {"column": 1}
     instance = model.from_dict(**model_dict)
-    assert instance.to_dict() == model_dict
+    session = sessionmaker()
+    session.add(instance)
+    session.flush()
+    queried_instance = session.query(model).first()
+    assert queried_instance.to_dict() == model_dict
 
 
 @pytest.mark.integration
-def test_to_from_dict_relationship():
+def test_to_from_dict_relationship(engine, sessionmaker):
     """
     GIVEN specification that has a schema with a relationship
     WHEN model is defined based on schema and constructed using from_dict
     THEN when to_dict is called the construction dictionary is returned.
     """
+    # Creating model factory
+    base = declarative.declarative_base()
     model_factory = open_alchemy.init_model_factory(
-        base=mock.MagicMock,
+        base=base,
         spec={
             "components": {
                 "schemas": {
                     "RefTable": {
-                        "properties": {"id": {"type": "integer"}},
-                        "x-tablename": "table",
+                        "properties": {
+                            "id": {"type": "integer", "x-primary-key": True}
+                        },
+                        "x-tablename": "ref_table",
                         "type": "object",
                     },
                     "Table": {
                         "properties": {
-                            "column": {"$ref": "#/components/schemas/RefTable"}
+                            "id": {"type": "integer", "x-primary-key": True},
+                            "ref_table": {"$ref": "#/components/schemas/RefTable"},
                         },
                         "x-tablename": "table",
                         "type": "object",
@@ -66,8 +80,14 @@ def test_to_from_dict_relationship():
     )
     model_factory(name="RefTable")
     model = model_factory(name="Table")
+    # Creating models
+    base.metadata.create_all(engine)
 
     # COnstructing and turning back to dictionary
-    model_dict = {"column": {"id": 1}}
+    model_dict = {"id": 11, "ref_table": {"id": 12}}
     instance = model.from_dict(**model_dict)
-    assert instance.to_dict() == model_dict
+    session = sessionmaker()
+    session.add(instance)
+    session.flush()
+    queried_instance = session.query(model).first()
+    assert queried_instance.to_dict() == model_dict
