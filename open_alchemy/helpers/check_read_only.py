0c074a747a3cc414bd5ced99321851faeae1cd68
David Andersson <anderssonpublic@gmail.com>
2019-11-14 08:53:22 +1100

    Add  and allOf resolution for arrays

--- open_alchemy/helpers/check_read_only.py
+++ open_alchemy/helpers/check_read_only.py
@@ -1,22 +1,29 @@
 """Check whether a property is readOnly."""
 
+import typing
+
 from open_alchemy import exceptions
 from open_alchemy import types
 
 from .get_ext_prop import get_ext_prop
+from .prepare_schema import prepare_schema
 
 
-def check_read_only(*, spec: types.Schema) -> bool:
+def check_read_only(
+    *, spec: types.Schema, schemas: typing.Optional[types.Schemas] = None
+) -> bool:
     """
     Check whether the specification is a valid read only.
 
     Assume allOf and $ref have been resolved.
 
+    Raise MissingArgumentError for array types when schemas is None.
     Raise MalformedSchemaError if the type is an object or array or not defined.
     Raise MalformedSchemaError if the x-backref-column is not defined.
 
     Args:
         spec: The spec to check.
+        schemas: Used to resolve any $ref and allOf for array items.
 
     Returns:
         Whether the property is a read only property.
@@ -38,12 +45,17 @@ def check_read_only(*, spec: types.Schema) -> bool:
             "readOnly properties cannot be an object."
         )
     if type_ == "array":
-        items = spec.get("items")
-        if items is None:
+        if schemas is None:
+            raise exceptions.MissingArgumentError(
+                "check_read_only for arrays must be called with schemas not None."
+            )
+        items_spec = spec.get("items")
+        if items_spec is None:
             raise exceptions.MalformedSchemaError(
                 "A readOnly array must define its items."
             )
-        items_type = items.get("type")
+        items_spec = prepare_schema(schema=items_spec, schemas=schemas)
+        items_type = items_spec.get("type")
         if items_type is None:
             raise exceptions.MalformedSchemaError(
                 "A readOnly array must define the type of its items."
--- tests/open_alchemy/helpers/test_check_read_only.py
+++ tests/open_alchemy/helpers/test_check_read_only.py
@@ -7,25 +7,34 @@ from open_alchemy import helpers
 
 
 @pytest.mark.parametrize(
-    "spec",
+    "spec, schemas",
     [
-        {"readOnly": True, "x-backref-column": "id"},
-        {"readOnly": True, "type": "object", "x-backref-column": "id"},
-        {"readOnly": True, "type": "array", "x-backref-column": "id"},
-        {"readOnly": True, "type": "array", "items": {}, "x-backref-column": "id"},
-        {
-            "readOnly": True,
-            "type": "array",
-            "items": {"type": "object"},
-            "x-backref-column": "id",
-        },
-        {
-            "readOnly": True,
-            "type": "array",
-            "items": {"type": "array"},
-            "x-backref-column": "id",
-        },
-        {"readOnly": True, "type": "simple_type"},
+        ({"readOnly": True, "x-backref-column": "id"}, None),
+        ({"readOnly": True, "type": "object", "x-backref-column": "id"}, None),
+        ({"readOnly": True, "type": "array", "x-backref-column": "id"}, {}),
+        (
+            {"readOnly": True, "type": "array", "items": {}, "x-backref-column": "id"},
+            {},
+        ),
+        (
+            {
+                "readOnly": True,
+                "type": "array",
+                "items": {"type": "object"},
+                "x-backref-column": "id",
+            },
+            {},
+        ),
+        (
+            {
+                "readOnly": True,
+                "type": "array",
+                "items": {"type": "array"},
+                "x-backref-column": "id",
+            },
+            {},
+        ),
+        ({"readOnly": True, "type": "simple_type"}, None),
     ],
     ids=[
         "no type",
@@ -38,22 +47,26 @@ from open_alchemy import helpers
     ],
 )
 @pytest.mark.helper
-def test_check_read_only_malformed(spec):
+def test_malformed(spec, schemas):
     """
     GIVEN malformed spec
     WHEN check_read_only is called
     THEN MalformedSchemaError is raised.
     """
     with pytest.raises(exceptions.MalformedSchemaError):
-        helpers.check_read_only(spec=spec)
+        helpers.check_read_only(spec=spec, schemas=schemas)
 
 
 @pytest.mark.parametrize(
-    "spec, expected_result",
+    "spec, schemas, expected_result",
     [
-        ({}, False),
-        ({"readOnly": False}, False),
-        ({"readOnly": True, "type": "simple_type", "x-backref-column": "id"}, True),
+        ({}, None, False),
+        ({"readOnly": False}, None, False),
+        (
+            {"readOnly": True, "type": "simple_type", "x-backref-column": "id"},
+            None,
+            True,
+        ),
         (
             {
                 "readOnly": True,
@@ -61,18 +74,64 @@ def test_check_read_only_malformed(spec):
                 "items": {"type": "simple_type"},
                 "x-backref-column": "id",
             },
+            {},
+            True,
+        ),
+        (
+            {
+                "readOnly": True,
+                "type": "array",
+                "items": {"$ref": "#/components/schemas/Items"},
+                "x-backref-column": "id",
+            },
+            {"Items": {"type": "simple_type"}},
+            True,
+        ),
+        (
+            {
+                "readOnly": True,
+                "type": "array",
+                "items": {"allOf": [{"type": "simple_type"}]},
+                "x-backref-column": "id",
+            },
+            {},
             True,
         ),
     ],
-    ids=["readOnly missing", "readOnly false", "readOnly true", "readOnly array true"],
+    ids=[
+        "readOnly missing",
+        "readOnly false",
+        "readOnly true",
+        "readOnly true array",
+        "readOnly true array items $ref",
+        "readOnly true array items allOf",
+    ],
 )
 @pytest.mark.helper
-def test_check_read_only(spec, expected_result):
+def test_valid(spec, schemas, expected_result):
     """
     GIVEN spec and expected check result
     WHEN check_read_only is called
     THEN the expected result is returned.
     """
-    result = helpers.check_read_only(spec=spec)
+    result = helpers.check_read_only(spec=spec, schemas=schemas)
 
     assert result == expected_result
+
+
+@pytest.mark.helper
+def test_array_schemas_none():
+    """
+    GIVEN array readOnly spec
+    WHEN check_read_only with None for schemas
+    THEN MissingArgumentError is raised.
+    """
+    spec = {
+        "readOnly": True,
+        "type": "array",
+        "items": {"type": "simple_type"},
+        "x-backref-column": "id",
+    }
+
+    with pytest.raises(exceptions.MissingArgumentError):
+        helpers.check_read_only(spec=spec)
