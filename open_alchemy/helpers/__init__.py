d78e712db0ebf32c7bd5ae51401f12aef8ebb8ad
David Andersson <anderssonpublic@gmail.com>
2019-11-17 15:08:10 +1100

    Remove read only helper

--- open_alchemy/helpers/__init__.py
+++ open_alchemy/helpers/__init__.py
@@ -2,7 +2,6 @@
 # pylint: disable=useless-import-alias
 
 from . import peek as peek
-from .check_read_only import check_read_only as check_read_only
 from .define_all import define_all as define_all
 from .get_ext_prop import get_ext_prop as get_ext_prop
 from .merge_all_of import merge_all_of as merge_all_of
