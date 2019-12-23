56f24d6b96d18f0c38e460a55b966148c4ce7d30
David Andersson <anderssonpublic@gmail.com>
2019-12-23 15:04:12 +1100

    Add valid test case

--- tests/open_alchemy/column_factory/array_ref/test_gather_array_artifacts.py
+++ tests/open_alchemy/column_factory/array_ref/test_gather_array_artifacts.py
@@ -64,13 +64,32 @@ from open_alchemy.column_factory import array_ref
     ],
 )
 @pytest.mark.column
-def test_handle_array_invalid(schema, schemas):
+def test_invalid(schema, schemas):
     """
     GIVEN array schema that is not valid and schemas
-    WHEN handle_array is called
+    WHEN gather_array_artifacts is called
     THEN MalformedRelationshipError is raised.
     """
     with pytest.raises(exceptions.MalformedRelationshipError):
         array_ref._gather_array_artifacts.gather_array_artifacts(
             schema=schema, schemas=schemas, logical_name="ref_schema"
         )
+
+
+@pytest.mark.column
+def test_valid():
+    """
+    GIVEN array schema and schemas
+    WHEN gather_array_artifacts is called with the schema and schemas
+    THEN artifacts from the array schema are returned.
+    """
+    schema = {"type": "array", "items": {"$ref": "#/components/schemas/RefSchema"}}
+    schemas = {"RefSchema": {"type": "object", "x-tablename": "ref_schema"}}
+
+    artifacts = array_ref._gather_array_artifacts.gather_array_artifacts(
+        schema=schema, schemas=schemas, logical_name="ref_schema"
+    )
+
+    assert artifacts.relationship.model_name == "RefSchema"
+    assert artifacts.spec == {"type": "object", "x-tablename": "ref_schema"}
+    assert artifacts.fk_column == "id"
