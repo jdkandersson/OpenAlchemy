2b59447f496dc6070ba427e76ebe5db158ef91e1
David Andersson <anderssonpublic@gmail.com>
2020-01-03 17:35:29 +1100

    Add example of non nullable many to one

---
+++ examples/relationship_many_to_one_not_nullable_models.py
@@ -0,0 +1,6 @@
+from open_alchemy import init_yaml
+
+init_yaml(
+    "relationship-many-to-one-not-nullable-example-spec.yml",
+    models_filename="relationship_many_to_one_not_nullable_models_auto.py",
+)
