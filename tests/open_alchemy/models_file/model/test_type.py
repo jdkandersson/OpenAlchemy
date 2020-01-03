cba6e2c7a7aad3b989e8358adc3de7eac438655c
David Andersson <anderssonpublic@gmail.com>
2020-01-03 16:12:45 +1100

    Add database test for type rules

--- tests/open_alchemy/models_file/model/test_type.py
+++ tests/open_alchemy/models_file/model/test_type.py
@@ -1,8 +1,16 @@
 """Tests for type_."""
-# pylint: disable=protected-access
+# pylint: disable=protected-access,unused-import
+
+import datetime
+import sys
+import typing
 
 import pytest
+import sqlalchemy
+import typeguard
+from sqlalchemy.ext import declarative
 
+import open_alchemy
 from open_alchemy import models_file
 
 
@@ -93,3 +101,134 @@ def test_dict(type_, expected_type):
     returned_type = models_file._model._type.typed_dict(artifacts=artifacts)
 
     assert returned_type == expected_type
+
+
+@pytest.mark.parametrize(
+    "type_, format_, nullable, required, generated, value",
+    [
+        ("integer", None, None, None, None, 1),
+        ("integer", "int32", None, None, None, 1),
+        ("integer", "int64", None, None, None, 1),
+        ("number", None, None, None, None, 1.0),
+        ("number", "float", None, None, None, 1.0),
+        ("string", None, None, None, None, "value 1"),
+        ("string", "password", None, None, None, "value 1"),
+        ("string", "byte", None, None, None, "value 1"),
+        ("string", "binary", None, None, None, b"value 1"),
+        ("string", "date", None, None, None, datetime.date(year=2000, month=1, day=1)),
+        (
+            "string",
+            "date-time",
+            None,
+            None,
+            None,
+            datetime.datetime(year=2000, month=1, day=1, hour=1, minute=1, second=1),
+        ),
+        ("boolean", None, None, None, None, True),
+        # nullable
+        ("integer", None, None, None, None, 1),
+        ("integer", None, None, None, None, None),
+        ("integer", None, True, None, None, 1),
+        ("integer", None, True, None, None, None),
+        ("integer", None, False, None, None, 1),
+        # required
+        ("integer", None, None, None, None, 1),
+        ("integer", None, None, None, None, None),
+        ("integer", None, None, True, None, 1),
+        ("integer", None, None, False, None, 1),
+        ("integer", None, None, False, None, None),
+        # generated
+        ("integer", None, None, None, None, 1),
+        ("integer", None, None, None, None, None),
+        ("integer", None, None, None, True, 1),
+        ("integer", None, None, None, False, 1),
+        ("integer", None, None, None, False, None),
+    ],
+    ids=[
+        "type integer format None      value int",
+        "type integer format int32     value int",
+        "type integer format int64     value int",
+        "type number  format None      value float",
+        "type number  format double    value float",
+        "type string  format None      value str",
+        "type string  format password  value str",
+        "type string  format byte      value str",
+        "type string  format binary    value bytes",
+        "type string  format date      value datetime.date",
+        "type string  format date-time value datetime.datetime",
+        "type boolean format None      value bool",
+        "nullable None  value not None",
+        "nullable None  value None",
+        "nullable True  value not None",
+        "nullable True  value None",
+        "nullable False value not None",
+        "required None  value not None",
+        "required None  value None",
+        "required True  value not None",
+        "required False value not None",
+        "required False value None",
+        "generated None  value not None",
+        "generated None  value None",
+        "generated True  value not None",
+        "generated False value not None",
+        "generated False value None",
+    ],
+)
+@pytest.mark.models_file
+def test_database_type_simple(
+    engine, sessionmaker, type_, format_, nullable, required, generated, value
+):
+    """
+    GIVEN simple type, format, nullable, required and a value
+    WHEN a specification is written for the combination and a model created and
+        initialized with the value
+    THEN the queried value complies with the type calculated by type_.model.
+    """
+    spec = {
+        "components": {
+            "schemas": {
+                "Table": {
+                    "properties": {
+                        "id": {
+                            "type": "integer",
+                            "x-primary-key": True,
+                            "x-autoincrement": True,
+                        },
+                        "column": {
+                            "type": type_,
+                            "format": format_,
+                            "nullable": nullable,
+                        },
+                    },
+                    "x-tablename": "table",
+                    "type": "object",
+                    # Use required to implement generated
+                    "required": ["column"] if required or generated else [],
+                }
+            }
+        }
+    }
+    # Creating model factory
+    base = declarative.declarative_base()
+    model_factory = open_alchemy.init_model_factory(spec=spec, base=base)
+    model = model_factory(name="Table")
+
+    # Create artifacts
+    artifacts = models_file.types.ColumnSchemaArtifacts(
+        type=type_, format=format_, nullable=nullable, required=required
+    )
+    calculated_type_str = models_file._model._type.model(artifacts=artifacts)
+    calculated_type = eval(calculated_type_str)  # pylint: disable=eval-used
+
+    # Creating models
+    base.metadata.create_all(engine)
+    # Creating model instance
+    model_instance = model(column=value)
+    session = sessionmaker()
+    session.add(model_instance)
+    session.flush()
+
+    # Querying session
+    queried_model = session.query(model).first()
+    assert queried_model.column == value
+    typeguard.check_type("queried_model.column", queried_model.column, calculated_type)
