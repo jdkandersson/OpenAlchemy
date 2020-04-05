455650f28b69cd078072ec82dc30d58b92eee770
David Andersson <anderssonpublic@gmail.com>
2020-04-05 13:45:35 +1000

    Correct type of parent model

--- open_alchemy/utility_base.py
+++ open_alchemy/utility_base.py
@@ -214,7 +214,7 @@ class UtilityBase:
         # Handle model that inherits
         if helpers.schema.inherits(schema=schema, schemas={}):
             # Retrieve parent model
-            parent: typing.Type = cls._get_parent(schema=schema)
+            parent: typing.Type[TUtilityBase] = cls._get_parent(schema=schema)
 
             # Get properties for schema
             properties = cls._get_properties()
