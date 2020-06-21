af1824a6982cca12f158d356f9db92000eaebc64
David Andersson <anderssonpublic@gmail.com>
2020-06-21 17:27:19 +1000

    Move id into test

--- tests/open_alchemy/models_file/model/test_type.py
+++ tests/open_alchemy/models_file/model/test_type.py
@@ -19,83 +19,111 @@ _ColSchemaArt = models_file.types.ColumnSchemaArtifacts
 @pytest.mark.parametrize(
     "artifacts, expected_type",
     [
-        pytest.param(_ColSchemaArt(type="integer", nullable=False), "int"),
         pytest.param(
-            _ColSchemaArt(type="integer", format="int32", nullable=False), "int"
+            _ColSchemaArt(type="integer", nullable=False),
+            "int",
+            id="integer no format",
         ),
         pytest.param(
-            _ColSchemaArt(type="integer", format="int64", nullable=False), "int"
-        ),
-        pytest.param(_ColSchemaArt(type="number", nullable=False), "float"),
-        pytest.param(
-            _ColSchemaArt(type="number", format="float", nullable=False), "float"
-        ),
-        pytest.param(_ColSchemaArt(type="string", nullable=False), "str"),
-        pytest.param(
-            _ColSchemaArt(type="string", format="password", nullable=False), "str"
+            _ColSchemaArt(type="integer", format="int32", nullable=False),
+            "int",
+            id="integer int32 format",
         ),
         pytest.param(
-            _ColSchemaArt(type="string", format="byte", nullable=False), "str"
+            _ColSchemaArt(type="integer", format="int64", nullable=False),
+            "int",
+            id="integer int64 format",
         ),
         pytest.param(
-            _ColSchemaArt(type="string", format="binary", nullable=False), "bytes"
+            _ColSchemaArt(type="number", nullable=False),
+            "float",
+            id="number no format",
         ),
         pytest.param(
-            _ColSchemaArt(type="string", format="date", nullable=False), "datetime.date"
+            _ColSchemaArt(type="number", format="float", nullable=False),
+            "float",
+            id="number float format",
+        ),
+        pytest.param(
+            _ColSchemaArt(type="string", nullable=False), "str", id="string no format",
+        ),
+        pytest.param(
+            _ColSchemaArt(type="string", format="password", nullable=False),
+            "str",
+            id="string password format",
+        ),
+        pytest.param(
+            _ColSchemaArt(type="string", format="byte", nullable=False),
+            "str",
+            id="string byte format",
+        ),
+        pytest.param(
+            _ColSchemaArt(type="string", format="binary", nullable=False),
+            "bytes",
+            id="string binary format",
+        ),
+        pytest.param(
+            _ColSchemaArt(type="string", format="date", nullable=False),
+            "datetime.date",
+            id="string date format",
         ),
         pytest.param(
             _ColSchemaArt(type="string", format="date-time", nullable=False),
             "datetime.datetime",
+            id="string date-time format",
         ),
-        pytest.param(_ColSchemaArt(type="boolean", nullable=False), "bool"),
+        pytest.param(
+            _ColSchemaArt(type="boolean", nullable=False),
+            "bool",
+            id="boolean no format",
+        ),
         pytest.param(
             _ColSchemaArt(type="object", nullable=False, de_ref="RefModel"),
             '"TRefModel"',
+            id="object",
         ),
         pytest.param(
             _ColSchemaArt(
                 type="object", nullable=False, de_ref="RefModel", default="value 1"
             ),
             '"TRefModel"',
+            id="object defult",
         ),
         pytest.param(
             _ColSchemaArt(type="array", de_ref="RefModel"),
             'typing.Sequence["TRefModel"]',
+            id="array",
         ),
         pytest.param(
             _ColSchemaArt(type="array", de_ref="RefModel", default="value 1"),
             'typing.Sequence["TRefModel"]',
+            id="array default",
         ),
-        pytest.param(_ColSchemaArt(type="integer"), "typing.Optional[int]"),
-        pytest.param(_ColSchemaArt(type="integer", required=True), "int"),
-        pytest.param(_ColSchemaArt(type="integer", generated=True), "int"),
         pytest.param(
-            _ColSchemaArt(type="integer", generated=False), "typing.Optional[int]"
+            _ColSchemaArt(type="integer"),
+            "typing.Optional[int]",
+            id="nullable and required None",
         ),
-        pytest.param(_ColSchemaArt(type="integer", generated=False, default=1), "int"),
-    ],
-    ids=[
-        "integer no format",
-        "integer int32 format",
-        "integer int64 format",
-        "number no format",
-        "number float format",
-        "string no format",
-        "string password format",
-        "string byte format",
-        "string binary format",
-        "string date format",
-        "string date-time format",
-        "boolean no format",
-        "object",
-        "object defult",
-        "array",
-        "array default",
-        "nullable and required None",
-        "nullable None required True",
-        "nullable None generated True",
-        "nullable None generated False",
-        "nullable None default given",
+        pytest.param(
+            _ColSchemaArt(type="integer", required=True),
+            "int",
+            id="nullable None required True",
+        ),
+        pytest.param(
+            _ColSchemaArt(type="integer", generated=True),
+            "int",
+            id="nullable None generated True",
+        ),
+        pytest.param(
+            _ColSchemaArt(type="integer", generated=False),
+            "typing.Optional[int]",
+            id="nullable None generated False",
+        ),
+        pytest.param(
+            _ColSchemaArt(type="integer", generated=False, default=1),
+            "int",
+            id="nullable None default given",
+        ),
     ],
 )
 @pytest.mark.models_file
