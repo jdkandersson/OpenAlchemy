b64f9d96265a9408de25817aa118f35acbe6775d
David Andersson <anderssonpublic@gmail.com>
2020-07-26 15:47:42 +1000

    Add function that iterates over the properties of a constructable schema

--- open_alchemy/schemas/helpers/iterate.py
+++ open_alchemy/schemas/helpers/iterate.py
@@ -10,7 +10,7 @@ def constructable(
     *, schemas: types.Schemas
 ) -> typing.Iterable[typing.Tuple[str, types.Schema]]:
     """
-    Create an iterable with all constructable schemas.
+    Create an iterable with all constructable schemas from all schemas.
 
     Args:
         schemas: The schemas to iterate over.
@@ -24,3 +24,35 @@ def constructable(
             continue
 
         yield name, schema
+
+
+def properties(
+    *, schema: types.Schema, schemas: types.Schemas
+) -> typing.Iterable[typing.Tuple[str, types.Schema]]:
+    """
+    Create an iterable with all properties of a schema from a constructable schema.
+
+    Args:
+        schema: The constructable schems.
+        schemas: All defined schemas (not just the constructable ones).
+
+    Returns:
+        An interator with all properties of a schema.
+
+    """
+    # Handle $ref
+    if schema.get("$ref") is not None:
+        _, ref_schema = helpers.ref.resolve(name="", schema=schema, schemas=schemas)
+        yield from properties(schema=ref_schema, schemas=schemas)
+
+    # Handle allOf
+    all_of = schema.get("allOf")
+    if all_of is not None:
+        for sub_schema in all_of:
+            yield from properties(schema=sub_schema, schemas=schemas)
+
+    # Handle simple case
+    schema_properties = schema.get("properties")
+    if schema_properties is None:
+        return
+    yield from schema_properties.items()
--- tests/open_alchemy/schemas/helpers/test_iterate.py
+++ tests/open_alchemy/schemas/helpers/test_iterate.py
@@ -9,31 +9,31 @@ from open_alchemy.schemas.helpers import iterate
     "schemas, expected_schemas",
     [
         pytest.param({}, [], id="empty"),
-        pytest.param({"schema_1": {}}, [], id="single not"),
+        pytest.param({"Schema1": {}}, [], id="single not"),
         pytest.param(
-            {"schema_1": {"x-tablename": "table 1"}},
-            [("schema_1", {"x-tablename": "table 1"})],
+            {"Schema1": {"x-tablename": "table 1"}},
+            [("Schema1", {"x-tablename": "table 1"})],
             id="single is",
         ),
-        pytest.param({"schema_1": {}, "schema_2": {},}, [], id="multiple none"),
+        pytest.param({"Schema1": {}, "Schema2": {},}, [], id="multiple none"),
         pytest.param(
-            {"schema_1": {"x-tablename": "table 1"}, "schema_2": {},},
-            [("schema_1", {"x-tablename": "table 1"}),],
+            {"Schema1": {"x-tablename": "table 1"}, "Schema2": {},},
+            [("Schema1", {"x-tablename": "table 1"}),],
             id="multiple first",
         ),
         pytest.param(
-            {"schema_1": {}, "schema_2": {"x-tablename": "table 2"},},
-            [("schema_2", {"x-tablename": "table 2"}),],
+            {"Schema1": {}, "Schema2": {"x-tablename": "table 2"},},
+            [("Schema2", {"x-tablename": "table 2"}),],
             id="multiple last",
         ),
         pytest.param(
             {
-                "schema_1": {"x-tablename": "table 1"},
-                "schema_2": {"x-tablename": "table 2"},
+                "Schema1": {"x-tablename": "table 1"},
+                "Schema2": {"x-tablename": "table 2"},
             },
             [
-                ("schema_1", {"x-tablename": "table 1"}),
-                ("schema_2", {"x-tablename": "table 2"}),
+                ("Schema1", {"x-tablename": "table 1"}),
+                ("Schema2", {"x-tablename": "table 2"}),
             ],
             id="multiple all",
         ),
@@ -50,3 +50,64 @@ def test_constructable(schemas, expected_schemas):
     returned_schemas = iterate.constructable(schemas=schemas)
 
     assert list(returned_schemas) == expected_schemas
+
+
+@pytest.mark.parametrize(
+    "schema, schemas, expected_properties",
+    [
+        pytest.param({}, {}, [], id="no properties"),
+        pytest.param({"properties": {}}, {}, [], id="empty properties"),
+        pytest.param(
+            {"properties": {"prop_1": "value 1"}},
+            {},
+            [("prop_1", "value 1")],
+            id="single property",
+        ),
+        pytest.param(
+            {"properties": {"prop_1": "value 1", "prop_2": "value 2"}},
+            {},
+            [("prop_1", "value 1"), ("prop_2", "value 2")],
+            id="multiple property",
+        ),
+        pytest.param(
+            {"$ref": "#/components/schemas/RefSchema"},
+            {"RefSchema": {"properties": {"prop_1": "value 1"}}},
+            [("prop_1", "value 1")],
+            id="$ref",
+        ),
+        pytest.param({"allOf": []}, {}, [], id="allOf empty"),
+        pytest.param(
+            {"allOf": [{"properties": {"prop_1": "value 1"}}]},
+            {},
+            [("prop_1", "value 1")],
+            id="allOf single",
+        ),
+        pytest.param(
+            {
+                "allOf": [
+                    {"properties": {"prop_1": "value 1"}},
+                    {"properties": {"prop_2": "value 2"}},
+                ]
+            },
+            {},
+            [("prop_1", "value 1"), ("prop_2", "value 2")],
+            id="allOf multiple",
+        ),
+        pytest.param(
+            {"allOf": [{"$ref": "#/components/schemas/RefSchema"}]},
+            {"RefSchema": {"properties": {"prop_1": "value 1"}}},
+            [("prop_1", "value 1")],
+            id="allOf $ref",
+        ),
+    ],
+)
+@pytest.mark.schemas
+def test_properties(schema, schemas, expected_properties):
+    """
+    GIVEN schema, schemas and expected properties
+    WHEN properties is called with the schema and schemas
+    THEN the expected name and property schema are returned.
+    """
+    returned_properties = iterate.properties(schema=schema, schemas=schemas)
+
+    assert list(returned_properties) == expected_properties
