afec84b9640921ca423763de5a2efd2abff15976
David Andersson <anderssonpublic@gmail.com>
2020-07-26 17:09:13 +1000

    Add function that determines whether a property defines a back reference

--- open_alchemy/schemas/__init__.py
+++ open_alchemy/schemas/__init__.py
@@ -1,1 +1,4 @@
 """Performs operations on the schemas to prepare them for further processing."""
+
+from . import backref
+from . import helpers
--- open_alchemy/schemas/backref.py
+++ open_alchemy/schemas/backref.py
@@ -1,16 +1,35 @@
 """Pre-process schemas by adding any back references into the schemas."""
 
-# from .. import types
+from .. import helpers
+from .. import types
 
-# def _defines_backref(schema: types.Schema, schemas: types.Schemas) -> bool:
-#     """
-#     Check whether the property schema defines a back reference.
 
-#     The following rules are used:
-#     1. if there is a $ref
+def _defines_backref(schema: types.Schema, *, schemas: types.Schemas) -> bool:
+    """
+    Check whether the property schema defines a back reference.
 
-#     Args:
-#         schema: The schema of the property.
-#         schemas: All the defined schemas.
+    The following rules are used:
+    1. if there is an items key, recursively call on the items value.
+    1. peek for x-backrefs on the schema and return True if found.
+    3. Return False.
 
-#     """
+    Args:
+        _: Placeholder for unused name argument.
+        schema: The schema of the property.
+        schemas: All the defined schemas.
+
+    Returns:
+        Whether the property defines a back reference.
+
+    """
+    # Handle items
+    items_schema = schema.get("items")
+    if items_schema is not None:
+        return _defines_backref(schema=items_schema, schemas=schemas)
+
+    # Peek for backref
+    backref = helpers.peek.backref(schema=schema, schemas=schemas)
+    if backref is not None:
+        return True
+
+    return False
