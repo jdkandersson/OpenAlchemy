a1c5784ab00958f16493ea6ed677c831dbd62653
David Andersson <anderssonpublic@gmail.com>
2020-08-01 15:12:22 +1000

    Add many-to-many tests

--- tests/open_alchemy/schemas/validation/relationship/test_full.py
+++ tests/open_alchemy/schemas/validation/relationship/test_full.py
@@ -942,14 +942,170 @@ from open_alchemy.schemas.validation.relationship import full
             (True, None),
             id="one-to-many foreign key defined right foreign key",
         ),
-        pytest.param(id="many-to-many source no tablename"),
-        pytest.param(id="many-to-many referenced no tablename"),
-        pytest.param(id="many-to-many source no primary key property"),
-        pytest.param(id="many-to-many source invalid primary key property"),
-        pytest.param(id="many-to-many source multiple primary key property"),
-        pytest.param(id="many-to-many referenced no primary key property"),
-        pytest.param(id="many-to-many referenced invalid primary key property"),
-        pytest.param(id="many-to-many referenced multiple primary key property"),
+        pytest.param(
+            {"properties": {"id": {"type": "integer", "x-primary-key": True}},},
+            {"type": "array", "items": {"$ref": "#/components/schemas/RefSchema"}},
+            {
+                "RefSchema": {
+                    "x-tablename": "ref_schema",
+                    "x-secondary": "schema_ref_schema",
+                    "properties": {"id": {"type": "integer", "x-primary-key": True,}},
+                }
+            },
+            (False, "referenced schema must define x-tablename"),
+            id="many-to-many referenced no tablename",
+        ),
+        pytest.param(
+            {
+                "x-tablename": True,
+                "properties": {"id": {"type": "integer", "x-primary-key": True}},
+            },
+            {"type": "array", "items": {"$ref": "#/components/schemas/RefSchema"}},
+            {
+                "RefSchema": {
+                    "x-tablename": "ref_schema",
+                    "x-secondary": "schema_ref_schema",
+                    "properties": {"id": {"type": "integer", "x-primary-key": True,}},
+                }
+            },
+            (False, "value of x-tablename must be a string"),
+            id="many-to-many referenced invalid tablename",
+        ),
+        pytest.param(
+            {
+                "x-tablename": "schema",
+                "properties": {"id": {"type": "integer", "x-primary-key": True}},
+            },
+            {"type": "array", "items": {"$ref": "#/components/schemas/RefSchema"}},
+            {
+                "RefSchema": {
+                    "x-secondary": "schema_ref_schema",
+                    "properties": {"id": {"type": "integer", "x-primary-key": True,}},
+                }
+            },
+            (False, "referenced schema must define x-tablename"),
+            id="many-to-many referenced no tablename",
+        ),
+        pytest.param(
+            {
+                "x-tablename": "schema",
+                "properties": {"id": {"type": "integer", "x-primary-key": True}},
+            },
+            {"type": "array", "items": {"$ref": "#/components/schemas/RefSchema"}},
+            {
+                "RefSchema": {
+                    "x-secondary": "schema_ref_schema",
+                    "x-tablename": True,
+                    "properties": {"id": {"type": "integer", "x-primary-key": True,}},
+                }
+            },
+            (False, "value of x-tablename must be a string"),
+            id="many-to-many referenced invalid tablename",
+        ),
+        pytest.param(
+            {"x-tablename": "schema", "properties": {"id": {"type": "integer"},},},
+            {"type": "array", "items": {"$ref": "#/components/schemas/RefSchema"}},
+            {
+                "RefSchema": {
+                    "x-tablename": "ref_schema",
+                    "x-secondary": "schema_ref_schema",
+                    "properties": {"id": {"type": "integer", "x-primary-key": True,}},
+                }
+            },
+            (False, "source schema must have a primary key"),
+            id="many-to-many source no primary key property",
+        ),
+        pytest.param(
+            {"x-tablename": "schema", "properties": {"id": {"x-primary-key": True},},},
+            {"type": "array", "items": {"$ref": "#/components/schemas/RefSchema"}},
+            {
+                "RefSchema": {
+                    "x-tablename": "ref_schema",
+                    "x-secondary": "schema_ref_schema",
+                    "properties": {"id": {"type": "integer", "x-primary-key": True,}},
+                }
+            },
+            (False, "id must define a type"),
+            id="many-to-many source invalid primary key property",
+        ),
+        pytest.param(
+            {
+                "x-tablename": "schema",
+                "properties": {
+                    "id": {"type": "integer", "x-primary-key": True},
+                    "name": {"type": "string", "x-primary-key": True},
+                },
+            },
+            {"type": "array", "items": {"$ref": "#/components/schemas/RefSchema"}},
+            {
+                "RefSchema": {
+                    "x-tablename": "ref_schema",
+                    "x-secondary": "schema_ref_schema",
+                    "properties": {"id": {"type": "integer", "x-primary-key": True,}},
+                }
+            },
+            (
+                False,
+                "many-to-many relationships currently only support single "
+                "primary key schemas",
+            ),
+            id="many-to-many source multiple primary key property",
+        ),
+        pytest.param(
+            {
+                "x-tablename": "schema",
+                "properties": {"id": {"type": "integer", "x-primary-key": True}},
+            },
+            {"type": "array", "items": {"$ref": "#/components/schemas/RefSchema"}},
+            {
+                "RefSchema": {
+                    "x-tablename": "ref_schema",
+                    "x-secondary": "schema_ref_schema",
+                    "properties": {"id": {"type": "integer"},},
+                }
+            },
+            (False, "referenced schema must have a primary key"),
+            id="many-to-many referenced no primary key property",
+        ),
+        pytest.param(
+            {
+                "x-tablename": "schema",
+                "properties": {"id": {"type": "integer", "x-primary-key": True}},
+            },
+            {"type": "array", "items": {"$ref": "#/components/schemas/RefSchema"}},
+            {
+                "RefSchema": {
+                    "x-tablename": "ref_schema",
+                    "x-secondary": "schema_ref_schema",
+                    "properties": {"id": {"x-primary-key": True,},},
+                }
+            },
+            (False, "RefSchema.id must define a type"),
+            id="many-to-many referenced invalid primary key property",
+        ),
+        pytest.param(
+            {
+                "x-tablename": "schema",
+                "properties": {"id": {"type": "integer", "x-primary-key": True}},
+            },
+            {"type": "array", "items": {"$ref": "#/components/schemas/RefSchema"}},
+            {
+                "RefSchema": {
+                    "x-tablename": "ref_schema",
+                    "x-secondary": "schema_ref_schema",
+                    "properties": {
+                        "id": {"type": "integer", "x-primary-key": True,},
+                        "name": {"type": "string", "x-primary-key": True,},
+                    },
+                }
+            },
+            (
+                False,
+                "many-to-many relationships currently only support single "
+                "primary key schemas",
+            ),
+            id="many-to-many referenced multiple primary key property",
+        ),
         pytest.param(
             {
                 "x-tablename": "schema",
@@ -963,6 +1119,7 @@ from open_alchemy.schemas.validation.relationship import full
                     "properties": {"id": {"type": "integer", "x-primary-key": True,}},
                 }
             },
+            (True, None),
             id="many-to-many valid",
         ),
     ],
