5ddb3a68267e723f7f0436cd07a11831b7ad7afb
David Andersson <anderssonpublic@gmail.com>
2020-08-04 19:01:50 +1000

    Add function that checks the basic structure of a property

--- open_alchemy/schemas/validation/property_/__init__.py
+++ open_alchemy/schemas/validation/property_/__init__.py
@@ -1,4 +1,41 @@
 """Validation for properties."""
 
+from .... import exceptions
+from .... import helpers
+from .... import types as oa_types
+from .. import types
 from . import relationship
 from . import simple
+
+_SUPPORTED_TYPES = {"integer", "number", "string", "boolean", "object", "array"}
+
+
+def check_type(schemas: oa_types.Schemas, schema: oa_types.Schema) -> types.Result:
+    """
+    Check whether the type of a property can be calculated.
+
+    Algorithm:
+    1. check that the type is present and valid
+    2. check that it is one of integer, number, string, boolean, object or array and
+    3. check that x-json, if defined, is valid.
+
+    Args:
+        schemas: All defined schemas used to resolve any $ref.
+        schema: The property schema to check.
+
+    Returns:
+        Whether thetype of the property can be determined.
+
+    """
+    try:
+        type_ = helpers.peek.type_(schema=schema, schemas=schemas)
+        if type_ not in _SUPPORTED_TYPES:
+            return types.Result(False, f"{type_} is not a supported type")
+        helpers.peek.json(schema=schema, schemas=schemas)
+
+    except (exceptions.MalformedSchemaError, exceptions.TypeMissingError) as exc:
+        return types.Result(False, f"malformed schema :: {exc}")
+    except exceptions.SchemaNotFoundError as exc:
+        return types.Result(False, f"reference :: {exc}")
+
+    return types.Result(True, None)
---
+++ tests/open_alchemy/schemas/validation/property_/test_property_.py
@@ -0,0 +1,95 @@
+"""Tests for the property validation."""
+
+import pytest
+
+from open_alchemy.schemas.validation import property_
+
+TESTS = [
+    pytest.param(
+        True,
+        {},
+        (False, "malformed schema :: The schema must be a dictionary. "),
+        id="not dictionary",
+    ),
+    pytest.param(
+        {},
+        {},
+        (False, "malformed schema :: Every property requires a type. "),
+        id="type not valid",
+    ),
+    pytest.param(
+        {"type": "not supported"},
+        {},
+        (False, "not supported is not a supported type"),
+        id="type not supported",
+    ),
+    pytest.param({"type": "integer"}, {}, (True, None), id="type integer"),
+    pytest.param({"type": "number"}, {}, (True, None), id="type number"),
+    pytest.param({"type": "string"}, {}, (True, None), id="type string"),
+    pytest.param({"type": "boolean"}, {}, (True, None), id="type boolean"),
+    pytest.param({"type": "object"}, {}, (True, None), id="type object"),
+    pytest.param({"type": "array"}, {}, (True, None), id="type array"),
+    pytest.param(
+        {"$ref": True},
+        {},
+        (False, "malformed schema :: The value of $ref must ba a string. "),
+        id="type $ref not string",
+    ),
+    pytest.param(
+        {"$ref": "#/components/schemas/RefSchema"},
+        {},
+        (False, "reference :: 'RefSchema was not found in schemas.' "),
+        id="type $ref missing",
+    ),
+    pytest.param(
+        {"$ref": "#/components/schemas/RefSchema"},
+        {"RefSchema": {"type": "integer"}},
+        (True, None),
+        id="type $ref",
+    ),
+    pytest.param(
+        {"allOf": True},
+        {},
+        (False, "malformed schema :: The value of allOf must be a list. "),
+        id="type allOf not array",
+    ),
+    pytest.param(
+        {"allOf": [True]},
+        {},
+        (False, "malformed schema :: The elements of allOf must be dictionaries. "),
+        id="type allOf elements not dict",
+    ),
+    pytest.param({"allOf": [{"type": "integer"}]}, {}, (True, None), id="type allOf"),
+    pytest.param(
+        {"type": "integer", "x-json": "True"},
+        {},
+        (False, "malformed schema :: The x-json property must be of type boolean. "),
+        id="x-json invalid",
+    ),
+    pytest.param({"type": "integer", "x-json": True}, {}, (True, None), id="x-json"),
+    pytest.param(
+        {"$ref": "#/components/schemas/RefSchema"},
+        {"RefSchema": {"type": "integer", "x-json": "True"}},
+        (False, "malformed schema :: The x-json property must be of type boolean. "),
+        id="x-json invalid $ref",
+    ),
+    pytest.param(
+        {"allOf": [{"type": "integer", "x-json": "True"}]},
+        {},
+        (False, "malformed schema :: The x-json property must be of type boolean. "),
+        id="x-json allOf",
+    ),
+]
+
+
+@pytest.mark.parametrize("schema, schemas, expected_result", TESTS)
+@pytest.mark.schemas
+def test_check_type(schema, schemas, expected_result):
+    """
+    GIVEN schema, schemas and expected result
+    WHEN check_type is called with the schema and schemas
+    THEN the expected result is returned.
+    """
+    returned_result = property_.check_type(schema=schema, schemas=schemas)
+
+    assert returned_result == expected_result
