e4b2e07fcc654ed13eaf4ea2ada0343d71449d31
David Andersson <anderssonpublic@gmail.com>
2020-08-09 10:06:03 +1000

    Define initial data model

--- open_alchemy/schemas/validation/types.py
+++ open_alchemy/schemas/validation/types.py
@@ -2,6 +2,8 @@
 
 import typing
 
+from ... import types
+
 
 class Result(typing.NamedTuple):
     """Result of checking a schema."""
@@ -13,3 +15,27 @@ class Result(typing.NamedTuple):
 
 
 OptResult = typing.Optional[Result]
+
+
+class _TModelBase(types.TypedDict, total=False):
+    """TModel base class for keys that might not be present."""
+
+    properties: typing.Dict[str, Result]
+
+
+class TModel(_TModelBase, total=True):
+    """Record validation result of a model."""
+
+    result: Result
+
+
+class _TSpecBase(types.TypedDict, total=False):
+    """TSpec base class for keys that might not be presents."""
+
+    models: typing.Dict[str, TModel]
+
+
+class TSpec(_TSpecBase, total=True):
+    """Record validation result for a specification."""
+
+    result: Result
