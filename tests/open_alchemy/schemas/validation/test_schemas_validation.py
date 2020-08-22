5b24f0df2cde07e098a8f5f18fecfce91e6c8fc9
David Andersson <anderssonpublic@gmail.com>
2020-08-22 14:54:41 +1000

    Fix linting problems

--- tests/open_alchemy/schemas/validation/test_schemas_validation.py
+++ tests/open_alchemy/schemas/validation/test_schemas_validation.py
@@ -5,8 +5,8 @@ import pytest
 from open_alchemy.schemas import validation
 
 CHECK_TESTS = [
-    pytest.param(True, (False, "schemas must be a dictionary"), id="schemas not dict",),
-    pytest.param({}, (True, None), id="schemas empty",),
+    pytest.param(True, (False, "schemas must be a dictionary"), id="schemas not dict"),
+    pytest.param({}, (True, None), id="schemas empty"),
     pytest.param(
         {True: {}},
         (False, "schemas keys must be strings, True is not"),
@@ -18,27 +18,27 @@ CHECK_TESTS = [
         id="schemas values not dict",
     ),
     pytest.param(
-        {True: {}, "Schema2": {},},
+        {True: {}, "Schema2": {}},
         (False, "schemas keys must be strings, True is not"),
         id="multiple model first key invalid",
     ),
     pytest.param(
-        {"Schema1": {}, True: {},},
+        {"Schema1": {}, True: {}},
         (False, "schemas keys must be strings, True is not"),
         id="multiple model second key invalid",
     ),
     pytest.param(
-        {"Schema1": True, "Schema2": {},},
+        {"Schema1": True, "Schema2": {}},
         (False, "the value of Schema1 must be a dictionary"),
         id="multiple model first value invalid",
     ),
     pytest.param(
-        {"Schema1": {}, "Schema2": True,},
+        {"Schema1": {}, "Schema2": True},
         (False, "the value of Schema2 must be a dictionary"),
         id="multiple model second value invalid",
     ),
     pytest.param(
-        {"Schema1": {}, "Schema2": {},}, (True, None), id="multiple model valid",
+        {"Schema1": {}, "Schema2": {}}, (True, None), id="multiple model valid",
     ),
 ]
 
