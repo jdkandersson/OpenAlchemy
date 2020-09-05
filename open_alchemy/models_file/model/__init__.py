b3f307d265344f4d90bc9324292e51bfb7f67de3
David Andersson <anderssonpublic@gmail.com>
2020-09-05 22:59:35 +1000

    Remove unnecessary code

--- open_alchemy/models_file/model/__init__.py
+++ open_alchemy/models_file/model/__init__.py
@@ -6,25 +6,7 @@ from open_alchemy import schemas
 from open_alchemy import types as oa_types
 
 from .. import artifacts as models_file_artifacts
-from . import artifacts as _artifacts
 from . import source as _source
-from . import type_ as _type
-
-
-def generate(*, schema: oa_types.Schema, name: str) -> str:
-    """
-    Generate the class source from the schema.
-
-    Args:
-        schema: The schema of the model.
-        name: The name of the model.
-
-    Returns:
-        The source code for the model class.
-
-    """
-    artifacts = _artifacts.from_schema(schema=schema, name=name)
-    return _source.generate(artifacts=artifacts)
 
 
 def from_artifacts(
