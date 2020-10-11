f8df25c74453e4c6e0c17bea67fdd763524887a0
David Andersson <anderssonpublic@gmail.com>
2020-10-11 20:00:34 +1100

    Add helper that checks whether the property requires an association table

--- tests/open_alchemy/schemas/test_association.py
+++ tests/open_alchemy/schemas/test_association.py
@@ -5,6 +5,49 @@ import pytest
 from open_alchemy.schemas import association
 
 
+class TestRequiresAssociation:
+    """Tests for _requires_association."""
+
+    # pylint: disable=protected-access
+
+    TESTS = [
+        pytest.param(
+            {"type": "integer"},
+            {},
+            False,
+            id="simple",
+        ),
+        pytest.param(
+            {"type": "object"},
+            {},
+            False,
+            id="relationship not many-to-many",
+        ),
+        pytest.param(
+            {"type": "array", "items": {"x-secondary": "association"}},
+            {},
+            True,
+            id="relationship many-to-many",
+        ),
+    ]
+
+    @staticmethod
+    @pytest.mark.parametrize("schema, schemas, expected_result", TESTS)
+    @pytest.mark.schemas
+    @pytest.mark.association
+    def test_(schema, schemas, expected_result):
+        """
+        GIVEN schema, schemas and expected result
+        WHEN _requires_association is called with the schema and schemas
+        THEN the expected result is returned.
+        """
+        returned_result = association._requires_association(
+            schema=schema, schemas=schemas
+        )
+
+        assert returned_result == expected_result
+
+
 class TestCalculatePropertySchema:
     """Tests for _calculate_property_schema."""
 
