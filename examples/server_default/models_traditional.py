2e0730ca4407e01709c7b346232cad9df6995f7f
David Andersson <nderssonpublic@gmail.com>
2020-11-08 18:16:11 +1100

    Add example

---
+++ examples/server_default/models_traditional.py
@@ -0,0 +1,12 @@
+import sqlalchemy as sa
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class Employee(Base):
+    """Person that works for a company."""
+
+    __tablename__ = "employee"
+    id = sa.Column(sa.Integer, primary_key=True)
+    name = sa.Column(sa.String, server_default=sa.text("Unknown"))
