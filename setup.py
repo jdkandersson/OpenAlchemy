2de81f762c6ba36e6865ab26159a06a32a6c71fd
David Andersson <nderssonpublic@gmail.com>
2020-11-15 11:36:12 +1100

    prepare for v2.0.0 release

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="OpenAlchemy",
-    version="1.6.0",
+    version="2.0.0",
     author="David Andersson",
     author_email="anderssonpublic@gmail.com",
     description="Maps an OpenAPI schema to SQLAlchemy models.",
