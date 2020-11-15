7b76f8f2015b96f12a0ab867f66230d571f33390
David Andersson <nderssonpublic@gmail.com>
2020-11-15 12:50:13 +1100

    Move to using pypa release action

--- tests/open_alchemy/helpers/test_peek/test_peek_extension.py
+++ tests/open_alchemy/helpers/test_peek/test_peek_extension.py
@@ -25,8 +25,6 @@ def test_autoincrement_wrong_type():
         ({}, None),
         ({"x-autoincrement": True}, True),
         ({"x-autoincrement": False}, False),
-        ({"x-openalchemy-autoincrement": False}, False),
-        ({"x-openalchemy-autoincrement": True}, True),
     ],
     ids=["missing", "true", "false"],
 )
