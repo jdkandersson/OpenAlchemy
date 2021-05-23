40f388736e3adb559e25c7038cdcf3ad8fa7a83e
David Andersson <nderssonpublic@gmail.com>
2021-05-23 20:14:47 +1000

    bump version to 2.5.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = "2021, David Andersson"
 author = "David Andersson"
 
 # The full version, including alpha/beta/rc tags
-release = "2.4.2"
+release = "2.5.0"
 
 
 # -- General configuration ---------------------------------------------------
